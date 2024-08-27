0bdaa9d56c74d051bceef8a74f1387d1b4aa655a
Daniel Egbeleke <daniel.egbeleke@moneta.ng>
2024-08-28 00:24:14 +0100

    update:server error vite config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js2/app.js"],
             refresh: true,
+            detectTls: road-trip-planner.fly.dev,
         }),
     ],
 });
