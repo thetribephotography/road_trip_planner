b5bc539c58b6dff2646c74d83dc65a3e549dbeb2
Daniel Egbeleke <daniel.egbeleke@moneta.ng>
2024-08-28 02:46:13 +0100

    fix:server public folder

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js2/app.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js2/app.js",
+                "public/css/app.css",
+                "public/js/app.js",
+                "public/img/MapData.png",
+                "public/svg/*.svg",
+            ],
             refresh: true,
             detectTls: "road-trip-planner.fly.dev",
         }),
     ],
 });
+
+
